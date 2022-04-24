98fb2814f7ecc78bf8c71fa93412659181da6ca1
Akshat Singh <95408545+CoderApple@users.noreply.github.com>
2022-04-24 08:01:41 +0530

    Multiple Components

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Navbar from './navbar';
+import Home from './home';
 
 function App() {
   const title = 'Welcome to the new blog';
@@ -8,18 +10,10 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        {/* <p>{ person }</p> */}
-
-        <p>{ 10 }</p>
-        <p>{ "hello, ninjas" }</p>
-        <p>{ [1, 2, 3, 4, 5] }</p>
-        <p>{ Math.random() * 10}</p>
-
-        <a href={ link }>Google Site</a>
+        <Home />
+        
       </div>
     </div>
   );
