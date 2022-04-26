5fca01a32de991cf3efeb787c26113b42f50d328
Akshat Singh <95408545+CoderApple@users.noreply.github.com>
2022-04-26 08:34:32 +0530

    Use Effect Dependies

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -8,6 +8,8 @@ const Home = () => {
         {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
         {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
     ]);
+
+    const [name, setName] = useState('mario');
     
     const handleDelete = (id) =>{
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -16,11 +18,14 @@ const Home = () => {
 
     useEffect(() => {
         console.log('use effect ran');
-    });
+        console.log(name);
+    }, [name]);
 
     return (
         <div className="home">
             <Bloglist blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            <button onClick={() => setName('luigi')}>change name</button>
+            <p>{ name }</p>
         </div>
     );
 }
