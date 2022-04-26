d34b5689a8cdaeac6630a384e4340268ea043108
Akshat Singh <95408545+CoderApple@users.noreply.github.com>
2022-04-26 07:36:51 +0530

    Reusing Components

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -6,12 +6,13 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
         {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'luigi', id: 3}
+        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
     ]);
     
     return (
         <div className="home">
             <Bloglist blogs={blogs} title="All Blogs!"/>
+            <Bloglist blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's Blogs"/>
         </div>
     );
 }
