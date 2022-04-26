90aaaf7191e8dc97806cc23b73987bc98a35edbd
Akshat Singh <95408545+CoderApple@users.noreply.github.com>
2022-04-26 09:41:35 +0530

    Conditional Loading Message

--- dojo-blog/src/home.js
+++ dojo-blog/src/home.js
@@ -4,19 +4,24 @@ import Bloglist from './bloglist';
 const Home = () => {
     
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
         .then(response => {
             return response.json();
         })
         .then((data) =>{
             setBlogs(data);
+            setIsPending(false);
+        }, 1000);
         })
     }, []);
 
     return (
         <div className="home">
+            {isPending && <div>Loading...</div>}
             {blogs && <Bloglist blogs={blogs} title="All Blogs!"/>}
         </div>
     );
