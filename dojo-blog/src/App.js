1a9cbf73e239ebf5ea5782b4d1768752048668e0
Akshat Singh <95408545+CoderApple@users.noreply.github.com>
2022-04-28 17:44:20 +0530

    some changes

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,10 +1,9 @@
-import Navbar from './navbar';
-import Home from './home';
+import Navbar from './Navbar';
+import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Create';
 
 function App() {
-  
   return (
     <Router>
       <div className="App">
