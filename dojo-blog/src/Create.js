b35af90c79b65a8eea5aaaaa25d6edd413b4ad9f
Akshat Singh <95408545+CoderApple@users.noreply.github.com>
2022-04-30 07:03:17 +0530

    Submit events

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -6,10 +6,17 @@ const Create = () => {
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
 
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    console.log(blog);
+  }
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSumbit}>
         <label>Blog Title:</label>
         <input 
           type="text"  
